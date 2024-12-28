c7c000b9c9050a3dd41cb917c83dedf92d98c549
Deepak kumar <123898692+KDeepak5673@users.noreply.github.com>
2024-12-29 00:39:58 +0530

    Update 0167-two-sum-ii-input-array-is-sorted.java

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0, n = numbers.length;; ++i) {
-            int x = target - numbers[i];
-            int l = i + 1, r = n - 1;
-            while (l < r) {
-                int mid = (l + r) >> 1;
-                if (numbers[mid] >= x) {
-                    r = mid;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            if (numbers[l] == x) {
-                return new int[] {i + 1, l + 1};
+        int left = 0; // Start pointer
+        int right = numbers.length - 1; // End pointer
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // Return 1-based indices
+            } else if (sum < target) {
+                left++; // Move left pointer to increase the sum
+            } else {
+                right--; // Move right pointer to decrease the sum
             }
         }
+
+        return new int[]{-1, -1}; // This should never be reached due to constraints
     }
 }
