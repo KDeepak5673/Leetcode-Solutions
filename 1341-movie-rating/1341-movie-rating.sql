81ec9442125b3e0557b2061c0c74bfe28971c78c
Deepak kumar <123898692+KDeepak5673@users.noreply.github.com>
2025-07-07 21:45:48 +0530

    Time: 1285 ms (42.08%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT name as results
+FROM Users
+WHERE user_id = (SELECT user_id FROM MovieRating JOIN Users USING(user_id) GROUP BY user_id ORDER BY COUNT(*) DESC , name ASC LIMIT 1)
+
+
+UNION ALL
+
+SELECT title as results
+FROM Movies
+WHERE movie_id = (SELECT movie_id FROM MovieRating JOIN Movies USING(movie_id) WHERE created_at BETWEEN "2020-02-01" AND "2020-02-29" GROUP BY movie_id ORDER BY AVG(rating) DESC , title ASC LIMIT 1)
+
