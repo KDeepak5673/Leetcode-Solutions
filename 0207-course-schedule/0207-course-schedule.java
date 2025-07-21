553efa649719bb5ffccf179b6a370da81e7ca805
Deepak kumar <123898692+KDeepak5673@users.noreply.github.com>
2025-07-21 14:34:26 +0530

    Time: 3 ms (99.82%), Space: 45 MB (89.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,31 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] inStack = new int[numCourses];
-
-        for(int i =0 ; i < numCourses ; i++){
-            graph.add(new ArrayList<>());
-        }
-
-        for(int[] pair : prerequisites){
-            int course = pair[0];
-            int prereq = pair[1];
-
-            graph.get(prereq).add(course);
-            inStack[course]++;
-
-
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int i = 0 ; i < numCourses ; i++){
-            if(inStack[i] == 0) queue.offer(i);
-        }
-
-        int vis = 0;
-
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            vis++;
-
-            for(int neg : graph.get(curr)){
-                inStack[neg]--;
-                if(inStack[neg] == 0  ) queue.offer(neg); 
+    public boolean isCycle(List<List<Integer>>list,boolean vis[],boolean stack[],int curr){
+        vis[curr]=true;
+        stack[curr]=true;
+        for(int nbr:list.get(curr)){
+            if(stack[nbr])return true;
+            if(!vis[nbr]){
+                if(isCycle(list,vis,stack,nbr))return true;
             }
         }
-
-        return vis == numCourses;
-        
-        
+        stack[curr]=false;
+        return false;
     }
-
-    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>list=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            list.add(new ArrayList<>());
+        }
+        boolean vis[]=new boolean[numCourses];
+        boolean stack[]=new boolean[numCourses];
+        for(int[]arr:prerequisites){
+            list.get(arr[1]).add(arr[0]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(isCycle(list,vis,stack,i))return false;
+            }
+        }
+        return true;
+    }
 }
